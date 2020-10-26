b8e37540d979eedda348dfc131854ac18a87ac77
grap3fruit <swkwon333@gmail.com>
2020-10-27 00:09:19 +0900

    add: Add TIL-20201026

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    TIL: ['2020-1025', '2020-1024'],
+    TIL: ['2020-1026', '2020-1025', '2020-1024'],
     Docusaurus: ['main', 'doc1', 'doc2', 'doc3', 'mdx'],
     // Features: [],
   },
